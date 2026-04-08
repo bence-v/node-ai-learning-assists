5678cdf22d7a6cfb4a0dcf487d3691a0c0b1487f
bence-v <vargabence47@gmail.com>
2026-04-08 09:30:32 +0200

    -Minor fixes

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -41,7 +41,7 @@ const errorHandler = (err,req,res,next) => {
 
     console.log('Error: ', {
         message: err.message,
-        stack: process.env.NODE_ENV == 'develompment' ? err.stack : undefined
+        stack: process.env.NODE_ENV === 'development' ? err.stack : undefined
     });
 
     res.status(statusCode).json({
